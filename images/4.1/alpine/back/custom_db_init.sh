69b41be470fd19f7bbfb62e2129f650567196688
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-06-03 14:36:48 +0200

    :sparkles: Use custom db init for Slack install
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

---
+++ images/4.1/alpine/back/custom_db_init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+log() {
+  echo "[$(date +%Y-%m-%dT%H:%M:%S%:z)] $@"
+}
+
+# ------------------------------------------------------------------------------
+# Custom database init script
+
+#########################################
+## SLACK
+#########################################
+
+log "Run contrib Slack plugin migrations to generate the new needed table"
+python manage.py migrate taiga_contrib_slack
+
+#########################################
